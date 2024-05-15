72fd204f67e86e689bc69985906e6033ef8fc25e
Pramod Madhubhashana <108177082+PramodMadhubhashana@users.noreply.github.com>
2024-05-15 19:17:41 +0530

    histrograme connect

--- lib/pages/histrograme.dart
+++ lib/pages/histrograme.dart
@@ -1,52 +1,78 @@
+import 'dart:ffi';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class Histrograme extends StatefulWidget {
-  const Histrograme({super.key});
+  const Histrograme({Key? key}) : super(key: key);
 
   @override
   State<Histrograme> createState() => _HistrogrameState();
 }
 
 class _HistrogrameState extends State<Histrograme> {
-  List<_SalesData> data = [
-    _SalesData('Jan', 500),
-    _SalesData('Feb', 28),
-    _SalesData('Mar', 34),
-    _SalesData('Apr', 32),
-    _SalesData('May', 45)
-  ];
+  List<_SalesData> data = [];
+
+  Future<void> _fetchDataFromFirestore() async {
+    try {
+      final CollectionReference capacityHistory =
+          FirebaseFirestore.instance.collection('sales');
+      QuerySnapshot querySnapshot = await capacityHistory.get();
+
+      List<_SalesData> newData = querySnapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return _SalesData(data['year'].toString(), data['sales'].toDouble());
+      }).toList();
+
+      setState(() {
+        data = newData;
+      });
+    } catch (e) {
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to fetch data from Firestore: $e'),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDataFromFirestore();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         SfCartesianChart(
-          title: ChartTitle(
-            text: "Pramodoo",
-          ),
-          primaryXAxis: CategoryAxis(),
-          legend: Legend(isVisible: true),
-          tooltipBehavior: TooltipBehavior(enable: true),
-          series: <CartesianSeries<_SalesData, String>>[
-            BarSeries<_SalesData, String>(
+            title: ChartTitle(
+              text: "Pramodoo",
+            ),
+            primaryXAxis: CategoryAxis(),
+            tooltipBehavior: TooltipBehavior(enable: true),
+            series: <CartesianSeries<_SalesData, String>>[
+              BarSeries<_SalesData, String>(
                 dataSource: data,
-                xValueMapper: (_SalesData sales, _) => sales.year,
+                xValueMapper: (_SalesData sales, _) => sales.date,
                 yValueMapper: (_SalesData sales, _) => sales.sales,
                 name: 'Sales',
-                // Enable data label
                 dataLabelSettings: const DataLabelSettings(isVisible: true),
-                color: Color.fromARGB(255, 68, 137, 255)),
-          ],
-        ),
+                color: Colors.blueAccent,
+              )
+            ]),
       ],
     );
   }
 }
 
 class _SalesData {
-  _SalesData(this.year, this.sales);
+  _SalesData(this.date, this.sales);
 
-  final String year;
+  final String date;
   final double sales;
 }
